88cefc8a55185797ac153c2d741dd8e457a90f3c
Navyam-Raushan <125335352+Navyam-Raushan@users.noreply.github.com>
2023-08-26 22:57:40 +0530

    ready for deployment added remove feature

--- web.py
+++ web.py
@@ -18,9 +18,18 @@ st.write("This app will help you to boost your productivity. "
          "You can monitor yourself.")
 
 st.subheader("Here is your todos for today..")
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    #  if checkbox is checked its true in session state
+    # providing key to each todo to access it uniquly.
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="", placeholder="Add a new todo...",
               on_change=add_todos,
               key="new_todo")
+# st.write("These are the keys in whole session.")
+# st.session_state
